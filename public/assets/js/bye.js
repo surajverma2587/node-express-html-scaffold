c5dc82c0868d8e90fd474fd32fd75abb4b17da90
Suraj Verma <surajverma@rentalcars.com>
2021-05-19 19:18:45 +0100

    fix heroku link

--- public/assets/js/bye.js
+++ public/assets/js/bye.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:3000";
-const BASE_URL = "https://radiant-meadow-12715.herokuapp.com/";
+const BASE_URL = "https://radiant-meadow-12715.herokuapp.com";
 
 const onReady = async () => {
   const response = await fetch(`${BASE_URL}/api/bye`);
